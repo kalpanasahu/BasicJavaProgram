2261b0e1889f2caf3a5d54a97e11b07eae035e62
12kalpana.sahu@gmail.com <kalpana.sahu@impelsys.com>
2019-09-30 21:05:26 +0530

    changes done

--- app/src/main/java/com/k/program/ArrayListTest.java
+++ app/src/main/java/com/k/program/ArrayListTest.java
@@ -1,7 +1,20 @@
 package com.k.program;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*Interview question
+* create arraylist and add ,print
+*
+*
+* */
 public class ArrayListTest {
     public static void main(String[] arg){
-
+        List<String> lst=new ArrayList<String>();
+        lst.add("kalpana");
+        lst.add("sahu");
+        for(String str:lst){
+            System.out.print(" name "+str);
+        }
     }
 }
